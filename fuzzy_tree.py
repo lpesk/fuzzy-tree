5d3c180d9a89d42cdcd6cb4bcbfa306905b450c0
pesk <laura.peskin@gmail.com>
2023-07-29 11:20:04 -0700

    add knots to vine; misc cleanup
    now skips nonexistent paths and uses a proper
    queue for breadth-first traversal.

--- fuzzy_tree.py
+++ fuzzy_tree.py
@@ -3,10 +3,11 @@
 import argparse
 import os
 
+from collections import deque
 from datetime import datetime
 from random import choice, random
 
-H_CONNECT = '╟⊹ '
+H_CONNECTS = ['╟⊹ ','╠⊹ ', '╬⊹ ', '\b⊹╫⊹ ']
 V_CONNECT = '╢'
 D_CONNECT = '╚╧╤╗'
 U_CONNECT = '╔╤╧╝'
@@ -15,30 +16,8 @@ COL_END = '\33[0m'
 DIACRITICS = list(map(chr, range(768, 879)))
 
 DAY = 60 * 60 * 24
-WEEK = 7 * DAY
-MONTH = 4 * WEEK
-
-def space(depth):
-    return ' ' * depth * len(H_CONNECT)
-
-def color(is_dir):
-    if is_dir:
-        return COL_GREEN
-    return COL_END
-
-def decorate(name, modified):
-    now = datetime.now().timestamp()
-    age = now - modified
-
-    def choose(prob):
-        if prob > random():
-            return choice(DIACRITICS)
-        return ''
-
-    if age < WEEK:
-        return name
-    else:
-        return ''.join([c + choose( (1.0/60.0) * (age // MONTH) ) for c in name])
+YEAR = 365.0 * DAY
+MONTH =  YEAR / 12.0
 
 class Tree:
     def __init__(self, root, max_depth, dirs_only):
@@ -49,16 +28,13 @@ class Tree:
         self._build()
 
     def _build(self):
-        current = []
-        if self.root.is_dir or not self.dirs_only:
-            current = [self.root]
-        while current and (self.max_depth < 0 or self.depth < self.max_depth):
-            future = []
-            for node in current:
-                node.get_children(self.dirs_only)
+        queue = deque([self.root])
+        while queue and (self.max_depth < 0 or self.depth < self.max_depth):
+            for i in range(len(queue)):
+                node = queue.popleft()
+                node.collect_children(self.dirs_only)
                 for child in node.children:
-                    future.append(child)
-            current = future
+                    queue.append(child)
             self.depth += 1
 
     def display(self):
@@ -68,32 +44,46 @@ class Node:
     def __init__(self, dirname, basename, depth):
         self.basename = basename
         self.dirname = dirname
-        self.depth = depth
         self.path = os.path.join(dirname, basename)
+        try:
+            os.stat(self.path)
+        except:
+            raise
+        self.depth = depth
         self.is_dir = os.path.isdir(self.path)
+        self.modified = os.path.getatime(self.path)
         self.children = []
 
-    def get_children(self, dirs_only):
+    def collect_children(self, dirs_only):
         children = []
         if not self.is_dir:
             return
         for name in os.listdir(self.path):
-            child = Node(self.path, name, self.depth+1)
-            if child.is_dir or not dirs_only:
-                children.append(Node(self.path, name, self.depth+1))
+            try:
+                child = Node(self.path, name, self.depth+1)
+                if (child.is_dir or not dirs_only):
+                    children.append(child)
+            except:
+                continue
         children.sort(key=lambda x: x.basename)
         self.children = children
 
     def display_subtree(self):
-        lines = self._display()
+        def space(depth):
+            return ' ' * depth * len(H_CONNECTS[0])
 
-        # Represent this node's descendents
+        # Draw this node
+        lines = ""
+        if self.depth > 0:
+            lines += f"{space(self.depth)}{str(self)}\n"
+        else:
+            lines += f"{U_CONNECT}\n"
+            lines += f"{str(self)}\n"
+
+        # Draw this node's descendents
         for (i, child) in enumerate(self.children):
             if i == 0:
                 lines += f"{space(self.depth)}{D_CONNECT}\n"
-##           uncomment for more vertical space between items
-#            elif not self.children[i-1].children:
-#                lines += f"{space(self.depth+1)}{V_CONNECT}\n"
 
             lines += child.display_subtree()
 
@@ -101,17 +91,32 @@ class Node:
                 lines += f"{space(self.depth)}{U_CONNECT}\n"
         return lines
 
-    def _display(self):
-        lines = ""
-        if self.depth > 0:
-            lines += f"{space(self.depth)}{H_CONNECT}{color(self.is_dir)}{decorate(self.basename, os.path.getatime(self.path))}{COL_END}\n"
-        else:
-            lines += f"{U_CONNECT}\n"
-            lines += f"{H_CONNECT}{color(self.is_dir)}{decorate(self.path, os.path.getatime(self.path))}{COL_END}\n"
-        return lines
+    def __str__(self):
+        now = datetime.now().timestamp()
+        age = now - self.modified
 
-    def __repr__(self):
-        return f"Node at depth {self.depth} with path {self.path}"
+        def choose(prob):
+            if prob > random():
+                return choice(DIACRITICS)
+            return ''
+
+        stem = H_CONNECTS[0]
+        name = self.basename if self.depth > 0 else self.path
+        col = COL_GREEN if self.is_dir else COL_END
+        if age > MONTH:
+            # Add progressively more random moss each month up to 5 years
+            name = ''.join([c + choose( (1.0/60.0) * (age // MONTH) ) for c in name])
+            # Make the vine deterministically knottier over time
+            if self.depth > 0:
+                if age > 2 * YEAR:
+                    stem = H_CONNECTS[3]
+                elif age > YEAR:
+                    stem = H_CONNECTS[2]
+                elif age > MONTH:
+                    stem = H_CONNECTS[1]
+
+        return f"{stem}{col}{name}{COL_END}"
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
